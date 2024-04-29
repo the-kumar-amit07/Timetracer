aab99317289cab873e6323ecb72425ebcd9bf995
the-kumar-amit07 <amitkumarmarndi37@gmail.com>
2024-04-29 09:49:09 +0530

    userprofile changes

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,6 +4,7 @@ import { MdOutlineCancel } from 'react-icons/md'
 
 import { Button } from '.'
 import avatar from '../data/avatar.jpeg'
+import { userProfileData } from '../data/dummy'
 const UserProfile = () => {
   return (
     <div className='nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96 '>
@@ -16,12 +17,32 @@ const UserProfile = () => {
         borderRadius='50%'
         />
       </div>
+      <div className='flex gap-5 items-center mt-6 border-color border-b-1 pb-6'>
+        <img className='rounded-full h-24 w-24' src={avatar} alt="user-profile" />
       <div>
         <p className='font-semibold text-xl dark:text-gray-200'>Amit Kumar Marndi</p>
-        <p className='text-gray-500 text-sm dark:text-gray-400'>Developer</p>
-        
+        <p className='text-gray-500 text-sm dark:text-gray-400'>Developer</p> 
       </div>
     </div>
+    <div>
+      {userProfileData.map((item,index) => (
+        <div key={index} 
+          className='flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer dark:hover:bg-[#42464D]' >
+          <button 
+          type='button'
+          style={{color:item.iconColor,backgroundColor:item.iconBg}}
+          className='text-xl rounded-lg p-3 hover:bg-light-gray'>
+            {item.icon}
+          </button>
+          <div>
+            <p className='font-semibold dark:text-gray-200'>{item.title}</p>
+            <p className='text-gray-500 text-sm dark:text-gray-400'>{item.desc}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+    </div>
+  
   )
 }
 
