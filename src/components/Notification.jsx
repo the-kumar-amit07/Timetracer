10f214621474afa90df6e610ec0c04f0f09ecc0a
the-kumar-amit07 <amitkumarmarndi37@gmail.com>
2024-05-01 14:58:17 +0530

    correction

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,9 +1,44 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Button from './Button'
+import { MdOutlineCancel } from 'react-icons/md'
+import { chatData } from '../data/dummy'
+import { useStateContext } from '../contexts/ContextProvider'
 
 const Notifiacation = () => {
+  const{currentColor} = useStateContext();
   return (
-    <div>Notifiacation</div>
+    <div className='nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96' >
+    <div className=''>
+      <div className=''>
+        <p>Notification</p>
+        <button type='button' className='text-white text-xs rounded p-1 px-2 bg-orange-theme'>5 New</button>
+      </div>
+      <Button icon={<MdOutlineCancel/>} 
+      color='rgba(153,171,180)' 
+      bgHoverColor='light-gray'
+      borderRadius= '50%'/>
+    </div>
+    <div  className='mt-5'>
+      {chatData?.map((item,index)=>(
+        <div key={index} className='flex items-center leading-8 gap-5 border-b-1 border-color p-3'>
+          <img className='rounded-full h-10 w-10' src={item.image} alt={item.message} />
+          <div>
+            <p className='font-semibold dark:text-gray-200'>{item.message}</p>
+            <p className='text-gray-500 text-sm dark:text-gray-400'>{item.desc}</p>
+          </div>
+        </div>
+      ))}
+      <div className='mt-5'>
+        <Button 
+        color= 'white'
+        bgColor={currentColor}
+        text='See all notifications'
+        borderRadius='10px'
+        width='full' />
+      </div>
+    </div>
+  </div>
   )
 }
 
