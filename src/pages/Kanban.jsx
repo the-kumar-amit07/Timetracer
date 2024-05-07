5a75a6c050d8af2b97f3452ebb556ec0d291e921
the-kumar-amit07 <amitkumarmarndi37@gmail.com>
2024-05-07 21:33:26 +0530

    doone

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,39 +1,9 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import React from 'react'
 
-const localizer = momentLocalizer(moment);
-
-const Kanban = () => {
-  const [events, setEvents] = useState([]);
-
-  const handleSelect = ({ start, end }) => {
-    const title = window.prompt('New Event name');
-    if (title) {
-      const newEvent = {
-        start,
-        end,
-        title,
-      };
-      setEvents([...events, newEvent]);
-    }
-  };
-
+function Kanban() {
   return (
-    <div>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        onSelectSlot={handleSelect}
-      />
-    </div>
-  );
-};
+    <div>Kanban</div>
+  )
+}
 
-export default Kanban;
-
+export default Kanban
