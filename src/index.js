b5abc283df1d2ecec6365ae07e8852efaa9f0331
the-kumar-amit07 <amitkumarmarndi37@gmail.com>
2024-05-26 10:14:53 +0530

    yes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ContextProvider } from './contexts/ContextProvider';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
 
 ReactDOM.render(
-  <React.StrictMode>
-
-  
-   <ContextProvider>
+  <>
+    <ContextProvider>
       <App />
     </ContextProvider>
-    
-
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </>,
+  document.getElementById("root")
 );
-
